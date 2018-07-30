7966db5a05a610741ff716d39459cca11eac836f
SDJX <SDJX@192.168.1.30>
2018-07-30 15:47:28 +0800

    第五次提交

--- src/cn/ps/Hello.java
+++ src/cn/ps/Hello.java
@@ -9,6 +9,7 @@ public class Hello {
 		System.out.println("Hello leilei!");
 		System.out.println("Hello java!");
 		System.out.println("Hello OOP!");
+		System.out.println("Hello IoStream!");
 	}
 
 }
